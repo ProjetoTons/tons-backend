eec4ef3888c0d30b5c92dc7a5a132b9898dcdc0c
Beatri_zss <beatriz.silva090@sptech.school>
2026-04-05 19:52:06 -0300

    criação de Dtos

---
+++ usuarios-ms/src/main/java/br/com/tonspersonalizados/usuarios_ms/dto/UsuarioRequestDto.java
@@ -0,0 +1,66 @@
+package br.com.tonspersonalizados.usuarios_ms.dto;
+
+import jakarta.validation.constraints.*;
+import org.hibernate.validator.constraints.br.CNPJ;
+import org.hibernate.validator.constraints.br.CPF;
+
+
+public class UsuarioRequestDto {
+
+    @NotBlank
+    @NotNull
+    private String nome;
+
+    @NotNull
+    @CPF
+    private String cpf;
+
+    @Email
+    private String email;
+
+    @NotNull
+    @Size(min = 11, max = 11)
+    private String telefone;
+
+
+    @NotNull
+    private String senha;
+
+    @CNPJ
+    private String cnpj;
+
+    //depois adicionar  mais validações para a senha de acordo com as regras que forem decididas.
+
+    private EnderecoRequestDto endereco;
+
+
+
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public EnderecoRequestDto getEndereco() {
+        return endereco;
+    }
+}
---
+++ usuarios-ms/src/main/java/br/com/tonspersonalizados/usuarios_ms/dto/UsuarioResponseDto.java
@@ -0,0 +1,8 @@
+package br.com.tonspersonalizados.usuarios_ms.dto;
+
+public class UsuarioResponseDto {
+    private String nome;
+    private String email;
+    private String telefone;
+
+}
